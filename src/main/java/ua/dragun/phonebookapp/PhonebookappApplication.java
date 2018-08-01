b2da1e58541dfa0a2de6e9f6dcbc4502dd53f1ed
maksdragun <MDragunWork@gmail.com>
2018-08-01 23:43:33 +0300

    #1 add generated Spring Boot project

---
+++ src/main/java/ua/dragun/phonebookapp/PhonebookappApplication.java
@@ -0,0 +1,12 @@
+package ua.dragun.phonebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookappApplication.class, args);
+	}
+}
