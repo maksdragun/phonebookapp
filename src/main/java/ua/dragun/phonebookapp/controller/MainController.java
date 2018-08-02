65d066106057087d618d385e7b98a4be529a0804
maksdragun <MDragunWork@gmail.com>
2018-08-02 16:24:04 +0300

    #9 app.properties name fix

--- src/main/java/ua/dragun/phonebookapp/controller/MainController.java
+++ src/main/java/ua/dragun/phonebookapp/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@PropertySource(value = {"classpath:file.properties"})
+@PropertySource(value = {"classpath:application.properties"})
 public class MainController {
 
     @RequestMapping("/*")
