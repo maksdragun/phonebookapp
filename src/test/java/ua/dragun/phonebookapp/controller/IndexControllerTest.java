24ae8000cd2b86bc0f5d279ce4152351120510d4
maksdragun <MDragunWork@gmail.com>
2018-08-07 13:41:18 +0300

    #18 add indexController test

---
+++ src/test/java/ua/dragun/phonebookapp/controller/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package ua.dragun.phonebookapp.controller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IndexControllerTest {
+	
+	@InjectMocks
+	private LoginController loginController;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	public void initialization(){
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(loginController).build();
+	}
+	
+	@Test
+	public void testWithNoSession() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(view().name("index"));
+	}
+	
+	@Test
+	public void testWithSession() throws Exception {
+		mockMvc.perform(get("/")
+				.sessionAttr("username", "username"))
+				.andExpect(view().name("redirect:/phoneBookApp"));
+	}
+}
